e938b391ce9bf23d9b9cb3e5a9664de7a68b6ba4
bt1159 <69985263+bt1159@users.noreply.github.com>
2026-02-02 09:46:30 -0500

    same

--- taskpane.js
+++ taskpane.js
@@ -69,29 +69,36 @@ async function createAndInsertImage() {
     }
 }
 
-await Excel.run(async (context) => {
-    let sheet = context.workbook.worksheets.getActiveWorksheet();
-    let dateTable = sheet.tables[0];
+async function getTableData() {
+    try {
 
-    // Get data from the header row.
-    let headerRange = expensesTable.getHeaderRowRange().load("values");
+        await Excel.run(async (context) => {
+            let sheet = context.workbook.worksheets.getActiveWorksheet();
+            let dateTable = sheet.tables[0];
 
-    // Get data from the table.
-    let bodyRange = expensesTable.getDataBodyRange().load("values");
+            // Get data from the header row.
+            let headerRange = expensesTable.getHeaderRowRange().load("values");
 
-    // Get data from a single column.
-    let columnRange = expensesTable.columns.getItem("Merchant").getDataBodyRange().load("values");
+            // Get data from the table.
+            let bodyRange = expensesTable.getDataBodyRange().load("values");
 
-    // Get data from a single row.
-    let rowRange = expensesTable.rows.getItemAt(1).load("values");
+            // Get data from a single column.
+            let columnRange = expensesTable.columns.getItem("Merchant").getDataBodyRange().load("values");
 
-    // Sync to populate proxy objects with data from Excel.
-    await context.sync();
+            // Get data from a single row.
+            let rowRange = expensesTable.rows.getItemAt(1).load("values");
 
-    let headerValues = headerRange.values;
-    let bodyValues = bodyRange.values;
-    let merchantColumnValues = columnRange.values;
-    let secondRowValues = rowRange.values;
+            // Sync to populate proxy objects with data from Excel.
+            await context.sync();
 
-    console.log('headerValues: ' + headerValues);
-});
+            let headerValues = headerRange.values;
+            let bodyValues = bodyRange.values;
+            let merchantColumnValues = columnRange.values;
+            let secondRowValues = rowRange.values;
+
+            console.log('headerValues: ' + headerValues);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
